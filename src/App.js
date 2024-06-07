95807cfd89002e9a9f8100b0d9f63bc5fffa1fd0
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-06-07 14:05:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { Scanner } from '@yudiel/react-qr-scanner';
+import { useState } from 'react';
 
-function App() {
+const customScannerStyles = {
+  container: {
+    width: '50%',
+    height: '400px', // Adjust height as needed
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  video: {
+    width: '100%',
+    height: '100%',
+  },
+};
+
+const App = () => {
+  const [scannedResult, setScannedResult] = useState('');
+
+  const handleScan = (result) => {
+    if (result && result.length > 0) {
+      setScannedResult(result[0].rawValue);
+    } else {
+      setScannedResult('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={customScannerStyles.container}>
+      <Scanner
+        onScan={handleScan}
+        styles={{
+          video: customScannerStyles.video,
+          container: customScannerStyles.container,
+        }}
+      />
+      <p>{scannedResult}</p>
     </div>
   );
-}
+};
 
 export default App;
